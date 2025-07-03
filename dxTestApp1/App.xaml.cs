0ee20df88845a905791f03b26d62a86414cc4adc
YuraSich <metelkiny98@gmail.com>
2025-07-03 13:54:40 +0300

    Добавьте файлы проекта.

---
+++ dxTestApp1/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace dxTestApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+    }
+
+}
---
+++ dxTestApp1/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Xpf.RichEdit;
+using System.Windows.Input;
+
+namespace dxTestApp1;
+
+class MainViewModel
+{
+    public ICommand SaveCommand => new DelegateCommand<RichEditControl>(Save);
+
+    private void Save(RichEditControl editor)
+    {
+        MessageBox.Show("Saved");
+    }
+}
