4adf2c7551b887d5cc9c22a4aec39be2a3350fad
YuraSich <metelkiny98@gmail.com>
2025-07-11 10:03:29 +0300

    fix

--- dxTestApp1/App.xaml.cs
+++ dxTestApp1/App.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace dxTestApp1
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : System.Windows.Application
-    {
-    }
+﻿using System.Globalization;
+using System.Windows;
 
+namespace dxTestApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : System.Windows.Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+        
+        base.OnStartup(e);
+    }
 }
--- dxTestApp1/MainViewModel.cs
+++ dxTestApp1/MainViewModel.cs
@@ -1,15 +1,31 @@
 ﻿using DevExpress.Mvvm;
 using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit;
+using System.IO;
 using System.Windows.Input;
 
 namespace dxTestApp1;
 
 class MainViewModel
 {
+    public Stream DocumentSource { get; set; }
+
+    public MainViewModel()
+    {
+        using var fs =  File.OpenRead("C:\\Users\\Yury\\Desktop\\file-sample_100kB.doc");
+        var ms = new MemoryStream();
+        fs.CopyTo(ms);
+        DocumentSource = ms;
+    }
+
     public ICommand SaveCommand => new DelegateCommand<RichEditControl>(Save);
 
     private void Save(RichEditControl editor)
     {
-        MessageBox.Show("Saved");
+        using var ms = new MemoryStream();
+        editor.Document.SaveDocument(ms, DocumentFormat.Doc);
+        using var fw = File.Create("C:\\Users\\Yury\\Desktop\\1.doc");
+        ms.Seek(0, SeekOrigin.Begin);
+        ms.CopyTo(fw);
     }
 }
